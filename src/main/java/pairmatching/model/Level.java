0c968bad2daa1ec68452fb11724d11e4d45751e9
minsulee <alkwen0996@naver.com>
2022-05-30 17:39:56 +0900

    feat: 레벨 입력 유효성 검사 기능 추가

--- src/main/java/pairmatching/model/Level.java
+++ src/main/java/pairmatching/model/Level.java
@@ -1,2 +1,31 @@
-package pairmatching.model;public class Level {
+package pairmatching.model;
+
+public enum Level {
+    LEVEL1("레벨1"),
+    LEVEL2("레벨2"),
+    LEVEL3("레벨3"),
+    LEVEL4("레벨4"),
+    LEVEL5("레벨5");
+
+    private String level;
+
+    Level(String level) {
+        this.level = level;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public static boolean isExist(String inputLevel){
+        final Level[] levels = Level.values();
+
+        for (Level level : levels) {
+            if (level.toString().equals(inputLevel)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
