25051f77da352727a72995080ddefc13ad03c3a6
minsulee <alkwen0996@naver.com>
2022-05-30 15:22:19 +0900

    feat: 기능 선택 메시지 출력 기능 추가

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,17 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String SELECT_FUNCTION_MESSAGE =
+        "기능을 선택하세요." + "\n" + "1. 페어 매칭" + "\n" + "2. 페어 조회" + "\n" + "3. 페어 초기화" + "\n" + "Q. 종료";
+
+    public void printSelectFunctionMessage() {
+        System.out.println(SELECT_FUNCTION_MESSAGE);
+    }
+
+    public String inputValue() {
+        return Console.readLine();
+    }
+}
