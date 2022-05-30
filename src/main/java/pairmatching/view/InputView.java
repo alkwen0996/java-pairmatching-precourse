425e8249559c7a1aa7f2efb7a366a0bf9855063f
minsulee <alkwen0996@naver.com>
2022-05-30 15:33:18 +0900

    feat: 입력 번호 유효성 검사 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -7,17 +7,23 @@ public class InputView {
     private static final String SELECT_FUNCTION_MESSAGE =
         "기능을 선택하세요." + "\n" + "1. 페어 매칭" + "\n" + "2. 페어 조회" + "\n" + "3. 페어 초기화" + "\n" + "Q. 종료";
 
-    public int selectFunctionNumber() {
-        printSelectFunctionMessage();
+    public static int selectFunctionNumber() {
+        try {
+            printSelectFunctionMessage();
 
-        return Integer.parseInt(inputValue());
+            return Integer.parseInt(inputValue());
+        }catch (IllegalArgumentException illegalArgumentException){
+            System.out.println(illegalArgumentException.getMessage());
+
+            return selectFunctionNumber();
+        }
     }
 
-    public void printSelectFunctionMessage() {
+    public static void printSelectFunctionMessage() {
         System.out.println(SELECT_FUNCTION_MESSAGE);
     }
 
-    public String inputValue() {
+    public static String inputValue() {
         return Console.readLine();
     }
 }
